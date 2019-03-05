8853f6b0030473771273cfb69def553a0a346929
TJ Ragsdale <tragsdale7@gmail.com>
2019-03-04 22:55:46 -0500

    build map

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const port = 3000;
 const data = require('./custom.geo.json')
 app.use(express.static(__dirname + '/'));
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
 	console.log('/ called');
 	res.send(index.html);
 });
 
-app.get('/custom.geo.json', function(req, res){
+app.get('/custom.geo.json', function(req, res) {
 	console.log('called!')
-	res.header("Content-Type",'application/json');
+	res.header("Content-Type", 'application/json');
 	res.send(data);
 });
 
